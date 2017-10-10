30f117ac8890cfde8e53c4f20807b884cd385c3b
Debbie Jong <surrealstardust@gmail.com>
2017-10-10 15:01:31 +0900

    finish model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Task.create(name: 'doctor', content: 'go to doctor for MRI scan')
+# Task.create(name: 'trash', content: 'take out trash by Wed night')
+# Task.create(name: 'flashcards', content: 'study le wagon flashcards by Friday')
